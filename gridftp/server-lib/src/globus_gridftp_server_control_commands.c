7bea5642d04b55d4836dc2569398c0f84fe5766c
Oliver Keeble <oliver.keeble@cern.ch>
2019-06-05 09:18:13 +0200

    Revert "patch from Andrey"
    This reverts commit 01561db059e30e3644f9ceb1b2e1d58fe6726476.

--- gridftp/server-lib/src/globus_gridftp_server_control_commands.c
+++ gridftp/server-lib/src/globus_gridftp_server_control_commands.c
@@ -2255,9 +2255,7 @@ globus_l_gsc_cmd_pasv(
     else if(strncmp(cmd_a[0], "EPSV", 4) == 0)
     {
         wrapper->dc_parsing_alg = 1;
-        wrapper->prt = op->server_handle->ipv6 ?
-                       GLOBUS_GRIDFTP_SERVER_CONTROL_PROTOCOL_IPV6 :
-                       GLOBUS_GRIDFTP_SERVER_CONTROL_PROTOCOL_IPV4;
+        wrapper->prt = GLOBUS_GRIDFTP_SERVER_CONTROL_PROTOCOL_IPV6;
         wrapper->max = 1;
         if(argc == 2)
         {
